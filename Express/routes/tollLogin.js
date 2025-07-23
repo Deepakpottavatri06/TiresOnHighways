db33018589e725b3a30c1c376a2da9b37fac7151
Deepakpottavatri06 <deepakpottavatri06@gmail.com>
2025-07-24 01:20:30 +0530

    set cookie changed

--- Express/routes/tollLogin.js
+++ Express/routes/tollLogin.js
@@ -43,7 +43,8 @@ router.post('/login', Tollupload.any(), async (req, res) => {
               httpOnly: false,   // For access via JS (optional, for debugging)
               secure: true,      // Required for cross-origin cookies in production
               sameSite: 'None',  // Required for cross-origin cookies
-               path: '/',            // Makes the cookie available across your entire site
+              path: '/',            // Makes the cookie available across your entire site
+              partitioned: 'true',
               maxAge: 60 * 60 * 1000,
             });
             // res.cookie('tollLogin', token, { domain: `${req.hostname}`, maxAge: 60 * 60 * 1000 });
